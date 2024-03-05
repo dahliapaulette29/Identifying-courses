de7efae2262fc50b81cb46b1545785c87b96764d
S <2304098@cognizant.com>
2024-03-05 15:34:01 +0530

    Hackathon-Project

--- src/test/java/identifycourses/Baseclass.java
+++ src/test/java/identifycourses/Baseclass.java
@@ -1,12 +1,17 @@
 package identifycourses;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -14,9 +19,9 @@ public class Baseclass {
 	public static WebDriver driver;
 	
 	@BeforeTest (groups= {"smoke","regression","master"})
-	public  void getDriver() throws InterruptedException
+	public  void getDriver() throws InterruptedException, Exception
 	{
-	    int browser;
+	    /*int browser;
 	    Scanner sc=new Scanner(System.in);
 	    System.out.println("Select a browser");
 		System.out.println("1.Chrome     2.Edge");
@@ -30,8 +35,15 @@ public class Baseclass {
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.coursera.org/");
 		Thread.sleep(5000);
+		driver.manage().window().maximize();*/
+		//http://10.66.138.113:4444
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setPlatform(Platform.WIN11);
+		capabilities.setBrowserName("MicrosoftEdge");
+		driver = new RemoteWebDriver(new URL("http://192.168.60.93:4444"),capabilities);
+		driver.get("https://www.coursera.org/");
+		Thread.sleep(5000);
 		driver.manage().window().maximize();
-		
 	
 	}
 	@AfterTest (groups= {"smoke","regression","master"})
